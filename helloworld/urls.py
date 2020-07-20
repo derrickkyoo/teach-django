3b3592bb5c0336beae089956e671321bd8423efd
Derrick Yoo <4998684+derrickyoo@users.noreply.github.com>
2020-07-19 18:59:45 -0700

    Implement django built in log in

--- helloworld/urls.py
+++ helloworld/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
     path("posts/", include("helloworld.posts.urls", namespace="posts"),),
     path("", include("helloworld.pages.urls", namespace="pages"),),
 ]
