41d695939a19818cf9b965531619fc1bb9589e83
Derrick Yoo <4998684+derrickyoo@users.noreply.github.com>
2020-07-20 21:01:21 -0700

    Implement post create

--- helloworld/posts/urls.py
+++ helloworld/posts/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
 
-from .views import PostListView, PostDetailView, PostUpdateView, PostDeleteView
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostUpdateView,
+    PostDeleteView,
+    PostCreateView,
+)
 
 app_name = "posts"
 urlpatterns = [
     path("<int:pk>/edit", PostUpdateView.as_view(), name="edit"),
     path("<int:pk>/delete", PostDeleteView.as_view(), name="delete"),
     path("<int:pk>/", PostDetailView.as_view(), name="detail"),
+    path("create/", PostCreateView.as_view(), name="create"),
     path("", PostListView.as_view(), name="list"),
 ]
--- helloworld/posts/views.py
+++ helloworld/posts/views.py
@@ -1,5 +1,11 @@
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, UpdateView, DeleteView
+from django.views.generic import (
+    ListView,
+    DetailView,
+    UpdateView,
+    DeleteView,
+    CreateView,
+)
 
 from .models import Post
 
@@ -34,3 +40,16 @@ class PostDeleteView(DeleteView):
     model = Post
     template_name = "posts/post_delete.html"
     success_url = reverse_lazy("posts:list")
+
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = "posts/post_create.html"
+    fields = (
+        "title",
+        "text",
+    )
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
